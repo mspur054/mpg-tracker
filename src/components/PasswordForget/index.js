0bfd016e283e99259abfb6f8eb8e904080e49dfd
mspur054 <mspur054@uottawa.ca>
2020-03-10 16:19:59 -0400

    pwd reset form validation added and now uses hooks

--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -3,61 +3,63 @@ import { Link } from "react-router-dom";
 import { withFirebase } from "../Firebase";
 import * as ROUTES from "../../constants/routes";
 
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 const PasswordForgetPage = () => (
   <div>
-    <h1>PasswordForget</h1>
+    <h1>Reset Password</h1>
     <PasswordForgetForm />
   </div>
 );
-const INITIAL_STATE = {
-  email: "",
-  error: null
+
+const validationSchema = Yup.object({
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email Required")
+});
+
+const PasswordHook = ({ firebase }) => {
+  const { handleSubmit, handleChange, handleBlur, errors } = useFormik({
+    initialValues: {
+      email: ""
+    },
+    validationSchema,
+    onSubmit: (values, { resetForm, setErrors }) => {
+      firebase
+        .doPasswordReset(values.email)
+        .then(() => {
+          resetForm();
+        })
+        .catch(error => {
+          setErrors({ email: error.message });
+        });
+    }
+  });
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="email"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        type="email"
+        placeholder="Email"
+      />
+      <p style={{ fontStyle: "italic" }}>
+        We will send you an email to reset your password
+      </p>
+      <button type="submit">Submit</button>
+      {errors && <p>{errors.email}</p>}
+    </form>
+  );
 };
-class PasswordForgetFormBase extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { ...INITIAL_STATE };
-  }
-  onSubmit = event => {
-    const { email } = this.state;
-    this.props.firebase
-      .doPasswordReset(email)
-      .then(() => {
-        this.setState({ ...INITIAL_STATE });
-      })
-      .catch(error => {
-        this.setState({ error });
-      });
-    event.preventDefault();
-  };
-  onChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-  render() {
-    const { email, error } = this.state;
-    const isInvalid = email === "";
-    return (
-      <form onSubmit={this.onSubmit}>
-        <input
-          name="email"
-          value={this.state.email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Address"
-        />
-        <button disabled={isInvalid} type="submit">
-          Reset My Password
-        </button>
-        {error && <p>{error.message}</p>}
-      </form>
-    );
-  }
-}
+
 const PasswordForgetLink = () => (
   <p>
     <Link to={ROUTES.PASSWORD_FORGET}>Forgot Password?</Link>
   </p>
 );
 export default PasswordForgetPage;
-const PasswordForgetForm = withFirebase(PasswordForgetFormBase);
+const PasswordForgetForm = withFirebase(PasswordHook);
 export { PasswordForgetForm, PasswordForgetLink };
