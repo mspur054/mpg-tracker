2d3197b1bc01c5538a3fc03a520d69a13ce32c17
mspur054 <mspur054@uottawa.ca>
2020-10-14 17:23:33 -0400

    added withUnits, context for unit selection

--- src/components/Session/index.js
+++ src/components/Session/index.js
@@ -1,5 +1,12 @@
 import AuthUserContext from "./context";
 import withAuthentication from "./withAuthentication";
 import withAuthorization from "./withAuthorization";
+import withUnits, { UnitContext } from "./withUnits";
 
-export { AuthUserContext, withAuthentication, withAuthorization };
+export {
+  AuthUserContext,
+  withAuthentication,
+  withAuthorization,
+  UnitContext,
+  withUnits,
+};
---
+++ src/components/Session/withUnits.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { compose } from "recompose";
+
+import { withFirebase } from "../Firebase";
+
+const UnitContext = React.createContext({
+  userSettings: {},
+});
+
+export { UnitContext };
+
+const withUnits = (Component) => {
+  class withUnits extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        userSettings: JSON.parse(localStorage.getItem("userSettings")),
+      };
+    }
+    componentDidMount() {
+      this.listener = this.props.firebase.auth.onAuthStateChanged(
+        (authUser) => {
+          const ref = this.props.firebase.db.ref(`/users/${authUser.uid}`);
+          ref.on("value", (snapshot) => {
+            const userSettings = snapshot.val();
+            localStorage.setItem("userSettings", JSON.stringify(userSettings));
+            this.setState({ userSettings });
+          });
+        },
+        () => {
+          localStorage.removeItem("userSettings");
+          this.setState({ userSettings: null });
+        }
+      );
+    }
+
+    componentWillUnmount() {
+      this.listener();
+    }
+
+    render() {
+      return (
+        <UnitContext.Provider value={this.state.userSettings}>
+          <Component {...this.props} />
+        </UnitContext.Provider>
+      );
+    }
+  }
+  return withFirebase(withUnits);
+};
+
+export default withUnits;
