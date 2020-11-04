052655b78a15b4ab05a1230ee212415893c380cb
mspur054 <mspur054@uottawa.ca>
2020-11-03 21:14:55 -0500

    cleaned up graph container, added metric/imperial

--- src/containers/GraphContainer/index.js
+++ src/containers/GraphContainer/index.js
@@ -7,27 +7,41 @@ import {
   StyledGraphContainer,
   StyledLabel,
 } from "./GraphContainer.Styled";
+import { kilometersToMiles } from "../../helper/efficency";
+
+const BAN = ({ banclass, display, label }) => {
+  return (
+    <StyledBAN className={banclass}>
+      {display}
+      <StyledLabel>{display === "N/A" ? "" : label}</StyledLabel>
+    </StyledBAN>
+  );
+};
 
 const GraphContainer = (props) => {
+  const costDisplay = props.totalCost ? `$${props.totalCost}` : "N/A";
+  const mpgDisplay = props.isMetric
+    ? !props.averageMetricMPG
+      ? "N/A"
+      : props.averageMetricMPG.toFixed(2)
+    : !props.averageMPG
+    ? "N/A"
+    : props.averageMPG.toFixed(2);
+  const distanceDisplay = !props.totalDistance
+    ? "N/A"
+    : props.isMetric
+    ? props.totalDistance
+    : kilometersToMiles(props.totalDistance);
+
   return (
     <StyledGridContainer>
-      <StyledBAN className="ban-spent">
-        {props.totalCost
-          ? `$${props.totalCost}` + <StyledLabel>Spent</StyledLabel>
-          : "N/A"}
-      </StyledBAN>
-      <StyledBAN className="ban-mpg">
-        {/* !TODO add KM / Miles */}
-        {props.averageMetricMPG
-          ? props.averageMetricMPG.toFixed(2) +
-            <StyledLabel>L/100 KM</StyledLabel>
-          : "N/A"}
-      </StyledBAN>
-      <StyledBAN className="ban-distance">
-        {/* !TODO add KM / Miles */}
-        {props.totalDistance ? props.totalDistance : "N/A"}
-        <StyledLabel>Tracked KM</StyledLabel>
-      </StyledBAN>
+      <BAN banclass="ban-spent" display={costDisplay} label="Spent" />
+      <BAN banclass="ban-mpg" display={mpgDisplay} label="L/100 KM" />
+      <BAN
+        banclass="ban-distance"
+        display={distanceDisplay}
+        label="Tracked KM"
+      />
       <StyledGraphContainer>
         <LineGraph entries={props.entries} />
       </StyledGraphContainer>
