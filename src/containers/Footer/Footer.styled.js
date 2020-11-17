c5638ef40538ffe5d3b837790cf10aeb48eba1a2
mspur054 <mspur054@uottawa.ca>
2020-11-16 19:41:19 -0500

    added dummy footer

--- src/containers/Footer/Footer.styled.js
+++ src/containers/Footer/Footer.styled.js
@@ -2,5 +2,49 @@ import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
   padding: 1rem;
-  // margin-top: auto;
+  width: 88%;
+  margin: auto;
+`;
+
+export const StyledOuterContainer = styled.div`
+  justify-content: stretch;
+  justify-items: start;
+  display: grid;
+  @media (min-width: 400px) {
+    grid-template-columns: repeat(6, 1fr);
+  }
+  @media screen and (min-width: 900px) {
+    grid-template-columns: repeat(12, 1fr);
+  }
+  grid-template-rows: auto;
+`;
+
+export const StyledAbout = styled.div`
+  text-align: left;
+  @media (min-width: 400px) {
+    grid-column: 1 / 6;
+  }
+  grid-column: 1 / 6;
+  padding-right: 15px;
+  padding-left: 15px;
+`;
+
+export const StyledLinks = styled.div`
+  
+  @media (min-width: 400px) {
+    grid-column: 1/6;
+    grid-row: auto;
+  }
+  @media screen and (min-width: 900px) {
+  grid-column 7 / 12;
+  }
+
+  grid-row: 1;
+  padding-right: 15px;
+  padding-left: 15px;
+
+  ul > li {
+    list-style-type: none;
+
+  }
 `;
--- src/containers/Footer/index.js
+++ src/containers/Footer/index.js
@@ -1,10 +1,30 @@
 import React from "react";
 
-import { StyledFooter } from "./Footer.styled";
+import {
+  StyledFooter,
+  StyledOuterContainer,
+  StyledAbout,
+  StyledLinks,
+} from "./Footer.styled";
 
 const Footer = () => {
   return (
     <StyledFooter>
+      <StyledOuterContainer>
+        <StyledAbout>
+          <h3>FUELLAGE</h3>
+          <p>Track your car's gas usage!</p>
+        </StyledAbout>
+        <StyledLinks>
+          <h3>LINKS</h3>
+          <ul>
+            <li>Link 1</li>
+            <li>Link 2</li>
+            <li>Link 3</li>
+          </ul>
+        </StyledLinks>
+      </StyledOuterContainer>
+
       <div>
         <p>
           Made with ❤ by{" "}
